910cda9c13417f9e591b114750ebb695deb2af5f
Gregory Dukes <kcils360@gmail.com>
2017-10-05 08:59:05 -0700

    finished in-order adn post-order

--- postOrder/postOrder/Program.cs
+++ postOrder/postOrder/Program.cs
@@ -6,7 +6,22 @@ namespace postOrder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Node root = new Node(1);
+            Tree tree = new Tree(root);
+            root.right = new Node(2);
+            root.left = new Node(3);
+            root.right.right = new Node(4);
+            root.right.left = new Node(5);
+            root.left.left = new Node(6);
+            root.left.right = new Node(7);
+
+            string InOrderString = "";
+            tree.PrintTree(root, ref InOrderString);
+
+            string PostOrderString = "";
+            tree.PostOrder(root, ref PostOrderString);
+            Console.Read();
         }
     }
 }
