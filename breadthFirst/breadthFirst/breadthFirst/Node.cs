1c595c4e63881a2c7a988556bafa9fda4e1099f1
Gregory Dukes <kcils360@gmail.com>
2017-10-05 17:10:37 -0700

    set up codes

---
+++ breadthFirst/breadthFirst/breadthFirst/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace breadthFirst
+{
+    class Node
+    {
+        public int Value;
+        public Node left;
+        public Node right;
+        public Node(int data)
+        {
+            Value = data;
+            left = null;
+            right = null;
+        }
+    }
